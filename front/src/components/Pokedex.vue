4a96585044e1155c60ee2eedbf11e189f17425d2
AndryRaza <razaandry974@gmail.com>
2022-05-03 09:19:58 +0400

    modif gitignore

---
+++ front/src/components/Pokedex.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <h1>Pokedex</h1>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name : "Pokedex",
+    data : function(){
+        return {
+
+        }
+    }
+}
+</script>
